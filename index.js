c919f03efe6d936368adee25d819ea9a13416223
tony-developer <anton.serebriackoff@yandex.ru>
2020-12-02 20:36:17 +0300

    first commit with routes and partials

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require('express')
+const app = express()
+const exphbs = require('express-handlebars')
+// const path = require('path')
+const homeRoutes = require('./routes/home')
+const addRoutes = require('./routes/add')
+const addCourses = require('./routes/courses')
+
+const hbs = exphbs.create({
+    defaultLayout: 'main',
+    extname: 'hbs'
+})
+
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', 'views')
+
+app.use(express.static('public'))
+app.use(express.urlencoded({
+    extended: false
+}))
+app.use('/', homeRoutes)
+app.use('/add', addRoutes)
+app.use('/courses', addCourses)
+
+// app.get('/', (req, res) => {
+//     // res.status(200)
+//     // res.sendFile(path.join(__dirname, 'views', 'index.html'))
+
+
+// })
+
+// app.get('/add', (req, res) => {
+//     // res.status(200)
+//     // res.sendFile(path.join(__dirname, 'views', 'about.html'))
+
+// })
+
+// app.get('/courses', (req, res) => {
+//     res.render('courses', {
+//         title: 'Курсы',
+//         isCourses: true
+//     })
+// })
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+    console.log(`Server id running on ${PORT}`)
+})
